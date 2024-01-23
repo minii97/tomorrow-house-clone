4f9cb3d5e097d884c04e3cc0fc8987d099863a15
minii97 <tmdalsqqz@gmail.com>
2024-01-23 15:27:16 +0900

    💄 Implement user-gallery's style and carousel function

--- js/carousel.js
+++ js/carousel.js
@@ -10,3 +10,16 @@ const productCarousel = tns({
   mouseDrag: true,
   preventScrollOnTouch: true,
 })
+
+const userGalleryMobile = tns({
+  container: '.user-gallery.is-mobile .slider-list',
+  loop: false,
+  gutter: 4,
+  edgePadding: 16,
+  controls: false,
+  navContainer: '.user-gallery.is-mobile .thumbnail-list',
+  navAsThumbnails: true,
+  arrowKeys: true,
+  mouseDrag: true,
+  preventScrollOnTouch: true,
+})
