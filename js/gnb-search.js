30370f898c9ddd2611504679c2233b21c2bbf7b9
minii97 <tmdalsqqz@gmail.com>
2024-01-29 16:02:23 +0900

    🚧 Implement deleting all gnb-search-history-items

--- js/gnb-search.js
+++ js/gnb-search.js
@@ -1,6 +1,13 @@
 const gnbSearch = document.querySelector('.gnb-search')
 const gnbSearchInput = gnbSearch.querySelector('input')
 const gnbSearchHistory = gnbSearch.querySelector('.search-history')
+const gnbSearchHistoryList = gnbSearchHistory.querySelector(
+  '.search-history-list'
+)
+
+const deleteAllBtn = gnbSearchHistory.querySelector(
+  '.search-history-header button'
+)
 
 function closeGnbSearchHistory(e) {
   if (!gnbSearch.contains(e.target)) {
@@ -10,8 +17,17 @@ function closeGnbSearchHistory(e) {
 }
 
 function openGnbSearchHistory() {
-  gnbSearchHistory.classList.add('is-active')
-  window.addEventListener('click', closeGnbSearchHistory)
+  if (gnbSearchHistoryList.children.length > 0) {
+    gnbSearchHistory.classList.add('is-active')
+    window.addEventListener('click', closeGnbSearchHistory)
+  }
 }
 
 gnbSearchInput.addEventListener('focus', openGnbSearchHistory)
+
+function clearGnbSearchHistory() {
+  gnbSearchHistory.classList.remove('is-active')
+  gnbSearchHistoryList.innerHTML = ''
+}
+
+deleteAllBtn.addEventListener('click', clearGnbSearchHistory)
