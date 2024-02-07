120c63e4754986a374411935c727904367d2ec6f
minii97 <tmdalsqqz@gmail.com>
2024-02-07 16:37:05 +0900

    🐛 Fix bugs on scroll event handler

--- js/product-tab.js
+++ js/product-tab.js
@@ -5,15 +5,20 @@ const TOP_HEADER_MOBILE = 50 + 40 + 40
 const TOP_HEADER_DESKTOP = 80 + 50 + 54
 
 let currentActiveTab = productTab.querySelector('.is-active')
+let disableUpdating = false
 
 function ActiveTab() {
-  // 1. li(Btn의 부모요소)에 is-active 클래스 추가
   const tabItem = this.parentNode
 
   if (currentActiveTab !== tabItem) {
+    disableUpdating = true
     tabItem.classList.add('is-active')
     currentActiveTab.classList.remove('is-active')
     currentActiveTab = tabItem
+
+    setTimeout(() => {
+      disableUpdating = false
+    }, 1000)
   }
 }
 
@@ -58,13 +63,14 @@ function detectTabPanelPosition() {
 
     productTabPanelPositionMap[id] = position
   })
+
+  updateActiveTabOnScroll()
 }
 
 function updateActiveTabOnScroll() {
-  //스크롤 위치에 따라서 active-tab 업데이트
-  // 1. 현재 유저가 얼마나 스크롤 했는가?
-  // 2. 요소가 문서 시작점으로부터 얼마나 아래에 있는가?
-  // -> productTabPanelPositionMap의 value로 저장해두었다.
+  if (disableUpdating) {
+    return
+  }
 
   const scrolledAmount =
     window.scrollY +
@@ -84,12 +90,21 @@ function updateActiveTabOnScroll() {
     newActiveTab = productTabBtnList[0] // 상품 정보 버튼
   }
 
+  const bodyHeight =
+    document.body.offsetHeight + (window.innerWidth < 1200 ? 56 : 0)
+
+  if (window.scrollY + window.innerHeight === bodyHeight) {
+    newActiveTab = productTabBtnList[4]
+  }
+
   if (newActiveTab) {
     newActiveTab = newActiveTab.parentNode
 
     if (newActiveTab !== currentActiveTab) {
       newActiveTab.classList.add('is-active')
-      currentActiveTab.classList.remove('is-active')
+      if (currentActiveTab !== null) {
+        currentActiveTab.classList.remove('is-active')
+      }
       currentActiveTab = newActiveTab
     }
   }
